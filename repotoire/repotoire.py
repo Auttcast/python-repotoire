d58e023b046fcda5788ba51ad2f5aa4a8556278f
autt <auttcast@gmail.com>
2025-06-19 16:34:29 -0500

    some work on type mapping

--- repotoire/repotoire.py
+++ repotoire/repotoire.py
@@ -1,9 +1,29 @@
 import dis
 import sqlite3
+from datetime import datetime, UTC
 from abc import ABC
 from typing import Callable, Iterable
 from auttcomp.extensions import Api as f
 from .expression_builder import Expression, ExpressionBuilder
+
+'''
+TODO
+-create with types
+-indexing
+
+TYPE MAP
+None -> NULL
+int -> INTEGER
+float -> REAL
+str -> TEXT
+bytes -> BLOG
+
+
+date:
+store as FLOAT_DATETIME
+adapter: datetime.now(UTC).timestamp()
+converter: datetime.fromtimestamp
+'''
 
 class Entity:
     rowid:int
@@ -13,8 +33,18 @@ class SqliteRepotoire(ABC):
     def __init__(self):
         self.connection:sqlite3.Connection
 
+    @staticmethod
+    def adapt_datetime_to_float(dt:datetime):
+        return dt.astimezone(UTC).timestamp()
+
+    @staticmethod
+    def convert_float_to_datetime(timestamp):
+        return datetime.fromtimestamp(timestamp)
+
     def connect(self, connection_string):
-        self.connection = sqlite3.connect(connection_string)
+        self.connection = sqlite3.connect(connection_string, detect_types=sqlite3.PARSE_DECLTYPES)
+        sqlite3.register_adapter(datetime, SqliteRepotoire.adapt_datetime_to_float)
+        sqlite3.register_converter("FLOAT_TIMESTAMP", SqliteRepotoire.convert_float_to_datetime)
 
     def __table_exists(self, cursor: sqlite3.Cursor, table_name:str):
         query = f"SELECT count(*) FROM sqlite_master WHERE [type]='table' and [name]='{table_name}'"
@@ -35,7 +65,7 @@ class SqliteRepotoire(ABC):
         cursor = self.connection.cursor()
 
         if not self.__table_exists(cursor, table_name):
-            cursor.execute(f"CREATE TABLE [{table_name}] ({SqliteRepotoire.escaped_props_str(properties)})")
+            cursor.execute(f"CREATE TABLE [{table_name}] ({self.escaped_typed_props_str(entity, properties)})")
         else:
             self.__assert_table_shape(cursor, table_name, properties)
 
@@ -45,6 +75,10 @@ class SqliteRepotoire(ABC):
     def escaped_props_str(props):
         return ",".join([f"[{x}]" for x in props])
 
+    def escaped_typed_props_str[T](self, entity:T, props:list[str]) -> str:
+        print(f"TEST123: {props} {type(getattr(entity, props[1]))}")
+        return ",".join([f"[{x}]" for x in props])
+
 class SqliteRepoApi[T]:
     def __init__(self, table_name:str, properties:list[str], entity_factory:Callable[[], T], cursor:sqlite3.Cursor):
         self.table_name:str = table_name
@@ -52,20 +86,13 @@ class SqliteRepoApi[T]:
         self.entity_factory:Callable[[], T] = entity_factory
         self.cursor:sqlite3.Cursor = cursor
 
-    def __null_adapter(self, prop):
-        if type(prop) is type(None):
-            return "NULL"
-        else:
-            return prop
-
     def add(self, entity:T):
         props = SqliteRepotoire.escaped_props_str(self.properties)
         prop_params = ",".join(["?" for _ in range(0, len(self.properties))])
         query = f"INSERT INTO [{self.table_name}] ({props}) VALUES ({prop_params})"
-        self.cursor.execute(query, [self.__null_adapter(getattr(entity, x)) for x in self.properties])
+        self.cursor.execute(query, [getattr(entity, x) for x in self.properties])
 
     def __to_entity(self, values) -> T:
-        print(f"VALUES: {type(values)}")
         obj:T = self.entity_factory()
         setattr(obj, 'rowid', values[0])
 
--- repotoire/testing/test_repotoire.py
+++ repotoire/testing/test_repotoire.py
@@ -23,15 +23,17 @@ def test_sqlite_repotoire_create():
 
     #act
 
-    repo.my_entity.add(TestEntity(name="test1", data_int=123))
-    repo.my_entity.add(TestEntity(name="test2"))
-
+    repo.my_entity.add(TestEntity(name="test1", data_int=123, data_float=1.1, data_datetime=datetime(2025, 1, 1)))
+    repo.my_entity.add(TestEntity(name="test2", data_int=456, data_float=1.02, data_datetime=datetime(2025, 1, 2)))
+    
     #assert
     native_result = repo.connection.execute("SELECT rowid, name, data_int, data_float, data_datetime FROM TestEntity").fetchall()
 
-    assert native_result[0][0] == 1
-    assert native_result[0][1] == 'test1'
-    assert native_result[0][2] == 123
+    assert native_result[0][0] == 1 #rowid
+    assert native_result[0][1] == 'test1' #name
+    assert native_result[0][2] == 123 #data_int
+    assert native_result[0][3] == 1.1 #data_float
+    assert native_result[0][4] == datetime(2025, 1, 1) #data_datetime
     
     assert native_result[1][0] == 2
     assert native_result[1][1] == 'test2'
@@ -41,3 +43,12 @@ def test_sqlite_repotoire_create():
 
 def test_sqlite_repotoire_query():
     pass
+
+def xtest_foo():
+    con = sqlite3.connect(":memory:")
+    con.execute(f"CREATE TABLE Test (foo, bar)")
+    con.execute("INSERT INTO Test (foo, bar) VALUES (NULL, 123)")
+    result = con.execute("SELECT * FROM Test").fetchall()
+    print(result)
+
+
