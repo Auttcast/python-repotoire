36e1b7ab8f12a092b075a13039da07c8a75fe09c
autt <auttcast@gmail.com>
2025-07-13 12:07:15 -0500

    cleanup and test_queryable_filter2_or

--- repotoire/testing/test_sqlite_query_builder.py
+++ repotoire/testing/test_sqlite_query_builder.py
@@ -118,15 +118,15 @@ def test_queryable_filter1():
     
     actual = comp()
 
+    assert len(actual) == 1
+
     e1 = actual[0]
 
     assert e1.rowid == 1
     assert e1.name == "foo1"
     assert e1.data == 123
-
-    assert actual == [e1]
     
-def test_queryable_filter2():
+def test_queryable_filter2_and():
 
     @dataclass
     class EntityA(Entity):
@@ -148,11 +148,45 @@ def test_queryable_filter2():
     
     actual = comp()
 
+    assert len(actual) == 1
+    
     e1 = actual[0]
 
     assert e1.rowid == 2
     assert e1.name == "foo2"
     assert e1.data == 456
-
-    assert actual == [e1]
     
+def test_queryable_filter2_or():
+
+    @dataclass
+    class EntityA(Entity):
+        name:str = None
+        data:int = None
+    
+    class TestRepo(SqliteRepotoire):
+        def __init__(self, connection_string):
+            super().connect(connection_string)
+            self.my_entity = super().register(EntityA)
+
+    repo = TestRepo(":memory:")
+
+    repo.my_entity.add(EntityA(name="foo1", data=123))
+    repo.my_entity.add(EntityA(name="foo2", data=456))
+    repo.my_entity.add(EntityA(name="foo3", data=789))
+    
+    comp = repo.my_entity.queryable() | q.filter(lambda x: x.data == 123 or x.data == 789) | q.list
+    
+    actual = comp()
+
+    assert len(actual) == 2
+    
+    e1 = actual[0]
+    e2 = actual[1]
+
+    assert e1.rowid == 1
+    assert e1.name == "foo1"
+    assert e1.data == 123
+
+    assert e2.rowid == 3
+    assert e2.name == "foo3"
+    assert e2.data == 789
